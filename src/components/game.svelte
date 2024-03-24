8b003c3a0110e9b2df668008efe55d4a5f64c3a3
Nicolás Rodríguez <nicrodriguezval@gmail.com>
2024-03-23 22:20:16 -0500

    Game interface & styles

---
+++ src/components/game.svelte
@@ -0,0 +1,27 @@
+<div class="game">
+	<div class="info"></div>
+	<div class="grid-container"></div>
+	<div class="info"></div>
+</div>
+
+<style>
+	.game {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+    height: 100vh;
+	}
+
+  .info {
+    width: 80vmin;
+    height: 10vmin;
+    background-color: #333;
+  }
+
+  .grid-container {
+    width: 80vmin;
+    height: 80vmin;
+    background-color: teal;
+  }
+</style>
